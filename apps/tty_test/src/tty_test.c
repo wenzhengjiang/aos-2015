54df5dc433546c34f47abcdc141d4685e8d5c27f
Chris Mann <chris@bitpattern.com.au>
2015-08-06 10:44:14 +1000

    Implement m0.

--- apps/tty_test/src/tty_test.c
+++ apps/tty_test/src/tty_test.c
@@ -39,11 +39,29 @@ thread_block(void){
 }
 
 int main(void){
-    /* initialise communication */
-    ttyout_init();
-
+    ssize_t count = 0;
+    const char* msg = "This is a message that's exactly 100 bytes long....................................................\n";
     do {
-        printf("task:\tHello world, I'm\ttty_test!\n");
+        count = printf("task:\tHello world, I'm\ttty_test!\n");
+        printf("count written: %d\n", count);
+        count = printf("Terminated early\n\0 DID NOT TERMINATE!\n");
+        printf("count written: %d\n", count);
+        count = printf("");
+        printf("count written: %d\n", count);
+        count = printf("%s%s", msg, msg);
+        printf("count written: %d\n", count);
+        count = printf("%s%s%s%s%s%s", msg, msg, msg, msg, msg, msg);
+        printf("count written: %d\n", count);
+        count = printf("\n");
+        count = printf("1\n");
+        count = printf("12\n");
+        count = printf("123\n");
+        count = printf("1234\n");
+        count = printf("12345\n");
+        count = printf("123456\n");
+        count = printf("1234567\n");
+        count = printf("12345678\n");
+        count = printf("123456789\n");
         thread_block();
         // sleep(1);	// Implement this as a syscall
     } while(1);
