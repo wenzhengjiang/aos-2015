a49f07dbd158592d16a297c482588681bdf2e06f
Chris Mann <chris@bitpattern.com.au>
2015-08-07 12:04:05 +1000

    Fix build.

--- libs/libclock/include/clock/clock.h
+++ libs/libclock/include/clock/clock.h
@@ -21,82 +21,8 @@
 #define CLOCK_R_CNCL (-2)       /* operation cancelled (driver stopped) */
 #define CLOCK_R_FAIL (-3)       /* operation failed for other reason */
 
-/* 
- * GPT Clock Register Settings
- */
-
-/* FORCE OUTPUT COMPARE on Output Compare 1 */
-const unsigned CLOCK_GPT_CR_FO1 = 1;
-/* Set the mode of comparison for output compare 1.  Active low-pulse mode is
- * configured. */
-const unsigned CLOCK_GPT_CR_OM1 = 4;
-
-/** FREE RUN OR RESTART MODE.  0 sets Restart mode that causes the counter to
- * reset when a compare is successful */
-const unsigned CLOCK_GPT_CR_FRR = 0;
-
-/* CLOCK SOURCE: 001 - Peripheral Clock */
-const unsigned CLOCK_GPT_CR_CLKSRC = 1;
-
-/* Enable mode */
-const unsigned CLOCK_GPT_CR_ENMOD = 1;
-
-/* Enable / disabled state */
-const unsigned CLOCK_GPT_CR_ENABLE = 1;
-const unsigned CLOCK_GPT_CR_DISABLE = 0;
-
-/* Free run or return restart mode */
-const unsigned CLOCK_GPT_CR_FRR_RESTART = 0;
-
-/* As the prescaler uses values 1..4096, where zero is illegal, they shift the
- * value mapping by one.  So need to subtract 1 to produce the desired value. */
-const unsigned CLOCK_GPT_PRESCALER = (66 - 1);
-
-/* GPT Interrupt register behaviours for output compare */
-const unsigned CLOCK_GPT_IR_OF1IE = 1;
-const unsigned CLOCK_GPT_IR_OF2IE = 0;
-const unsigned CLOCK_GPT_IR_OF3IE = 0;
-
-/* The physical starting address of the GPT for memory mapping */
 #define CLOCK_GPT_PADDR 0x2098000
-
 #define GPT_IRQ 87
-
-typedef uint64_t timestamp_t;
-typedef char byte_t;
-typedef void (*timer_callback_t)(uint32_t id, void *data);
-
-// TODO: How do we ensure the compiler doesn't modify the struct layout?
-struct gpt_control_register {
-    byte_t enable;
-    byte_t enable_mode;
-    byte_t debug_mode;
-    byte_t wain_mode;
-    byte_t doze_mode;
-    byte_t stop_mode_enabled;
-    byte_t clock_source[3];
-    byte_t free_run_or_restart;
-    byte_t reserved[4];
-    byte_t software_reset;
-    byte_t input_operating_mode_1[2];
-    byte_t input_operating_mode_2[2];
-    byte_t output_compare_mode_1[3];
-    byte_t output_compare_mode_2[3];
-    byte_t output_compare_mode_3[3];
-    byte_t force_output_compare_1;
-    byte_t force_output_compare_2;
-    byte_t force_output_compare_3;
-};
-
-struct gpt_interrupt_register {
-    byte_t output_compare_1_enable;
-    byte_t output_compare_2_enable;
-    byte_t output_compare_3_enable;
-    byte_t input_capture_1_enable;
-    byte_t input_capture_2_enable;
-    byte_t rollover_interrupt_enable;
-    byte_t reserved[26];
-};
 
 struct gpt_register_set {
     uint32_t control;
@@ -111,14 +37,8 @@ struct gpt_register_set {
     uint32_t counter;
 };
 
-struct timer_callback {
-    uint32_t  id;
-    uint64_t  next_timeout;
-    timer_callback_t callback;
-    void *data;
-};
-
-#define CALLBACK_ARRAY_LENGTH 1024
+typedef uint64_t timestamp_t;
+typedef void (*timer_callback_t)(uint32_t id, void *data);
 
 void clock_set_device_address(void* mapping);
 
--- libs/libclock/src/clock.c
+++ libs/libclock/src/clock.c
@@ -4,12 +4,93 @@
 #include <assert.h>
 #include <string.h>
 
+#define CALLBACK_ARRAY_LENGTH 1024
+
+/* 
+ * GPT Clock Register Settings
+ */
+
+/* FORCE OUTPUT COMPARE on Output Compare 1 */
+const unsigned CLOCK_GPT_CR_FO1 = 1;
+/* Set the mode of comparison for output compare 1.  Active low-pulse mode is
+ * configured. */
+const unsigned CLOCK_GPT_CR_OM1 = 4;
+
+/** FREE RUN OR RESTART MODE.  0 sets Restart mode that causes the counter to
+ * reset when a compare is successful */
+const unsigned CLOCK_GPT_CR_FRR = 0;
+
+/* CLOCK SOURCE: 001 - Peripheral Clock */
+const unsigned CLOCK_GPT_CR_CLKSRC = 1;
+
+/* Enable mode */
+const unsigned CLOCK_GPT_CR_ENMOD = 1;
+
+/* Enable / disabled state */
+const unsigned CLOCK_GPT_CR_ENABLE = 1;
+const unsigned CLOCK_GPT_CR_DISABLE = 0;
+
+/* Free run or return restart mode */
+const unsigned CLOCK_GPT_CR_FRR_RESTART = 0;
+
+/* As the prescaler uses values 1..4096, where zero is illegal, they shift the
+ * value mapping by one.  So need to subtract 1 to produce the desired value. */
+const unsigned CLOCK_GPT_PRESCALER = (66 - 1);
+
+/* GPT Interrupt register behaviours for output compare */
+const unsigned CLOCK_GPT_IR_OF1IE = 1;
+const unsigned CLOCK_GPT_IR_OF2IE = 0;
+const unsigned CLOCK_GPT_IR_OF3IE = 0;
+
+/* The physical starting address of the GPT for memory mapping */
+typedef char byte_t;
+
+// TODO: How do we ensure the compiler doesn't modify the struct layout?
+struct gpt_control_register {
+    byte_t enable;
+    byte_t enable_mode;
+    byte_t debug_mode;
+    byte_t wain_mode;
+    byte_t doze_mode;
+    byte_t stop_mode_enabled;
+    byte_t clock_source[3];
+    byte_t free_run_or_restart;
+    byte_t reserved[4];
+    byte_t software_reset;
+    byte_t input_operating_mode_1[2];
+    byte_t input_operating_mode_2[2];
+    byte_t output_compare_mode_1[3];
+    byte_t output_compare_mode_2[3];
+    byte_t output_compare_mode_3[3];
+    byte_t force_output_compare_1;
+    byte_t force_output_compare_2;
+    byte_t force_output_compare_3;
+};
+
+struct gpt_interrupt_register {
+    byte_t output_compare_1_enable;
+    byte_t output_compare_2_enable;
+    byte_t output_compare_3_enable;
+    byte_t input_capture_1_enable;
+    byte_t input_capture_2_enable;
+    byte_t rollover_interrupt_enable;
+    byte_t reserved[26];
+};
+
+struct timer_callback {
+    uint32_t  id;
+    uint64_t  next_timeout;
+    timer_callback_t callback;
+    void *data;
+};
+
 struct gpt_register_set* gpt_register_set;
 static uint32_t tick_count = 0;
 static struct timer_callback callbacks[CALLBACK_ARRAY_LENGTH];
 static seL4_CPtr _timer_cap = seL4_CapNull;
 void* gpt_clock_addr;
 static uint32_t clock_tick_count;
+
 
 /* TODO: we shouldn't do this */
 void clock_set_device_address(void* mapping) {
