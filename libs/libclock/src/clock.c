36edfc66f83810ffb2390112406d52a466ad4e20
Wenzheng Jiang <jwzh.hi@gmail.com>
2015-08-08 14:51:33 +1000

    changed to array timers

--- libs/libclock/src/clock.c
+++ libs/libclock/src/clock.c
@@ -6,302 +6,94 @@
 #include <sel4/types.h>
 #include <assert.h>
 #include <string.h>
-//#include <sync/mutex.h>
+#include <sync/mutex.h>
 
-static const timestamp_t max_tick = (1ULL<<63) - 1;
+#define MAX_CALLBACK_ID 20
+
+static const timestamp_t MAX_TICK = (1ULL<<63) - 1;
 
 struct callback {
+    bool valid;
     uint32_t id;
     uint64_t next_timeout;
     uint64_t delay;
     timer_callback_t fun;
     void *data;
-    struct callback *next;
 };
+
 typedef struct callback callback_t;
 
-static callback_t *callback_list;
-//static sync_mutex_t callback_m;
-
-static uint32_t gid;
-//static sync_mutex_t gid_m;
+static callback_t callback_arr[MAX_CALLBACK_ID+1];
+static sync_mutex_t callback_m;
 
 static bool initialized;
 
 static timestamp_t last_tick;
 
-/* 
- * GPT Clock Register Settings
- */
-
-/* FORCE OUTPUT COMPARE on Output Compare 1 */
-const unsigned CLOCK_GPT_CR_FO1 = 1;
-/* Set the mode of comparison for output compare 1.  Active low-pulse mode is
- * configured. */
-const unsigned CLOCK_GPT_CR_OM1 = 4;
-
-/** FREE RUN OR RESTART MODE.  0 sets Restart mode that causes the counter to
- * reset when a compare is successful */
-const unsigned CLOCK_GPT_CR_FRR = 0;
-
-/* CLOCK SOURCE: 001 - Peripheral Clock */
-const unsigned CLOCK_GPT_CR_CLKSRC = 1;
-
-/* Enable mode */
-const unsigned CLOCK_GPT_CR_ENMOD = 1;
-
-/* Enable / disabled state */
-const unsigned CLOCK_GPT_CR_ENABLE = 1;
-const unsigned CLOCK_GPT_CR_DISABLE = 0;
-
-/* Free run or return restart mode */
-const unsigned CLOCK_GPT_CR_FRR_RESTART = 0;
-
-/* As the prescaler uses values 1..4096, where zero is illegal, they shift the
- * value mapping by one.  So need to subtract 1 to produce the desired value. */
-const unsigned CLOCK_GPT_PRESCALER = (66 - 1);
-
-/* GPT Interrupt register behaviours for output compare */
-const unsigned CLOCK_GPT_IR_OF1IE = 1;
-const unsigned CLOCK_GPT_IR_OF2IE = 0;
-const unsigned CLOCK_GPT_IR_OF3IE = 0;
-
-/* The physical starting address of the GPT for memory mapping */
-typedef char byte_t;
-
-// TODO: How do we ensure the compiler doesn't modify the struct layout?
-struct gpt_control_register {
-    byte_t enable;
-    byte_t enable_mode;
-    byte_t debug_mode;
-    byte_t wain_mode;
-    byte_t doze_mode;
-    byte_t stop_mode_enabled;
-    byte_t clock_source[3];
-    byte_t free_run_or_restart;
-    byte_t reserved[4];
-    byte_t software_reset;
-    byte_t input_operating_mode_1[2];
-    byte_t input_operating_mode_2[2];
-    byte_t output_compare_mode_1[3];
-    byte_t output_compare_mode_2[3];
-    byte_t output_compare_mode_3[3];
-    byte_t force_output_compare_1;
-    byte_t force_output_compare_2;
-    byte_t force_output_compare_3;
-};
-
-struct gpt_interrupt_register {
-    byte_t output_compare_1_enable;
-    byte_t output_compare_2_enable;
-    byte_t output_compare_3_enable;
-    byte_t input_capture_1_enable;
-    byte_t input_capture_2_enable;
-    byte_t rollover_interrupt_enable;
-    byte_t reserved[26];
-};
-
-struct timer_callback {
-    uint32_t  id;
-    uint64_t  next_timeout;
-    timer_callback_t callback;
-    void *data;
-};
-
-struct gpt_register_set* gpt_register_set;
-static uint32_t tick_count = 0;
-static seL4_CPtr _timer_cap = seL4_CapNull;
-void* gpt_clock_addr;
-static uint32_t clock_tick_count;
-
-uint64_t get_uniq_id() {
-    //sync_acquire(gid_m); 
-    int ret = ++gid; // TODO better solusion ?
-    //sync_release(gid_m);
-
-    return ret;
-};
-
-/* Insert a new node while maintaining the increasing order of next_timou*/
-
-static void cblist_add(callback_t *new) {
-    //sync_acquire(callback_m); 
-
-    callback_t *cur = callback_list, *prev = NULL;
-    while (cur && cur->next_timeout <= new->next_timeout) {
-        prev = cur;
-        cur = cur->next;
-    }
-    new->next = NULL;
-    if (!prev) {            // insert at head
-        new->next = callback_list;
-        callback_list = new;
-    } else {                // insert after prev
-        prev->next = new;
-        new->next = cur;
-    }
-
-    //sync_release(callback_m);
-}
-
-/*
- * @return  CLOCK_R_OK if item removed successfully 
- *          CLOCK_R_FAIL if item was not found */
-static int cblist_remove(uint32_t id) {
-    //sync_acquire(callback_m); 
-
-    callback_t *cur = callback_list, *prev = NULL;
-    while (cur && cur->id != id) {
-        prev = cur;
-        cur = cur->next;
-    }
-    int ret = CLOCK_R_OK;
-    if (!cur) ret = CLOCK_R_FAIL; // not found
-    else {
-        if (!prev)
-            callback_list = callback_list->next; // remove head
-        else
-            prev->next = cur->next;
-        free(cur);
-    }
-
-    //sync_release(callback_m);
-    return ret;
-}
-
-static void cblist_destroy() {
-    callback_t * p = callback_list, *next = NULL;
-    while (p) {
-        next = p->next;
-        free(p);
-        p = next;
-    }
-}
-
-static seL4_CPtr
-enable_irq(int irq, seL4_CPtr aep) {
-    seL4_CPtr cap;
-    int err;
-    /* Create an IRQ handler */
-    cap = cspace_irq_control_get_cap(cur_cspace, seL4_CapIRQControl, irq);
-    assert(cap);
-    /* Assign to an end point */
-    err = seL4_IRQHandler_SetEndpoint(cap, aep);
-    assert(!err);
-    /* Ack the handler before continuing */
-    err = seL4_IRQHandler_Ack(cap);
-    assert(!err);
-    return cap;
-}
-
-int start_timer(seL4_CPtr interrupt_ep) {
-    struct gpt_control_register* gpt_control_register = &(gpt_register_set->control);
-    struct gpt_interrupt_register* gpt_interrupt_register = &(gpt_register_set->interrupt);
-    //if (!(callback_m = sync_create_mutex())) 
-    //    return CLOCK_R_FAIL;
-    //if (!(gid_m = sync_create_mutex()))
-    //    return CLOCK_R_FAIL;
-
+int start_timer(seL4_CPtr interrupt_ep)
+{
+    (void)interrupt_ep;
+    callback_m = sync_create_mutex();
     initialized = true;
-
-    gpt_register_set = gpt_clock_addr;
-    _timer_cap = enable_irq(GPT_IRQ, interrupt_ep);
-
-    assert(sizeof(gpt_control_register) == 4);
-
-    /* Ensure the clock is stopped */
-    gpt_control_register->enable = CLOCK_GPT_CR_DISABLE;
-
-    /* Configure the clock */
-    gpt_control_register->enable_mode = CLOCK_GPT_CR_ENMOD;
-    memcpy(gpt_control_register->clock_source, &CLOCK_GPT_CR_OM1, 3);
-    gpt_control_register->free_run_or_restart = CLOCK_GPT_CR_FRR_RESTART;
-    memcpy(gpt_control_register->output_compare_mode_1, &CLOCK_GPT_CR_OM1, 3);
-    gpt_control_register->force_output_compare_1 = CLOCK_GPT_CR_FO1;
-
-    gpt_register_set->prescaler = CLOCK_GPT_PRESCALER;
-
-    gpt_interrupt_register->output_compare_1_enable = CLOCK_GPT_IR_OF1IE;
-    gpt_interrupt_register->output_compare_2_enable = CLOCK_GPT_IR_OF2IE;
-    gpt_interrupt_register->output_compare_3_enable = CLOCK_GPT_IR_OF3IE;
-
-    assert(gpt_interrupt_register->reserved == 0);
-
-    /* Enable the clock */
-    gpt_control_register->enable = CLOCK_GPT_CR_ENABLE;
-
     return 0;
 }
 
-/* TODO: we shouldn't do this */
-void clock_set_device_address(void* mapping) {
-    gpt_clock_addr = mapping;
-}
-
-/*
- * Register a callback to be called after a given delay
- *    delay:  Delay time in microseconds before callback is invoked
- *    callback: Function to be called
- *    data: Custom data to be passed to callback function
- *
- * Returns 0 on failure, otherwise an unique ID for this timeout
- */
-
 uint32_t register_timer(uint64_t delay, timer_callback_t callback_fun, void *data) {
-    if (!initialized) return 0;
-    callback_t * cb = (callback_t*)malloc(sizeof(callback_t));
-    if (!cb)
-        return 0; 
-    
-    cb->id = get_uniq_id();
-    cb->delay = delay;
-    timestamp_t curtick = time_stamp();
-    if (max_tick - curtick < delay) {
-        cb->next_timeout = max_tick; // workaround for overflow
-    } else {
-        cb->next_timeout = curtick + delay;
+    if (callback_fun == NULL) {
+        dprintf(0, "invalid callback_fun\n");
+        return 0;
     }
-    cb->fun = callback_fun;
-    cb->data = data;
-    
-    cblist_add(cb);
-
-    return cb->id;
+    if (!initialized) {
+        dprintf(0, "timer hasn't been initialised \n");
+        return 0;
+    }
+    timestamp_t curtick = time_stamp();
+    callback_t *cb = NULL;
+    for (int i = 1;i <= MAX_CALLBACK_ID; i++) {
+        if (!callback_arr[i].valid) {
+            cb = &callback_arr[i];
+            cb->valid = true;
+            cb->id = i;
+            cb->delay = delay;
+            if (MAX_TICK - curtick < delay) {
+                cb->next_timeout = MAX_TICK; // workaround for overflow
+            } else {
+                cb->next_timeout = curtick + delay;
+            }
+            cb->fun = callback_fun;
+            cb->data = data;
+            break;
+        }
+    }
+    if (cb) return cb->id;
+    else return 0;
 }
-
-/*
- * Remove a previously registered callback by its ID
- *    id: Unique ID returned by register_time
- * Returns CLOCK_R_OK iff successful.
- */
 
 int remove_timer(uint32_t id) {
-    return cblist_remove(id);
+    if (id == 0 && id > MAX_CALLBACK_ID)
+        return CLOCK_R_FAIL;
+    if (!callback_arr[id].valid)
+        return CLOCK_R_FAIL;
+    callback_arr[id].valid = false;
+    return CLOCK_R_OK;
 }
-
-/*
- * Handle an interrupt message sent to 'interrupt_ep' from start_timer
- *
- * Returns CLOCK_R_OK iff successful
- */
 
 int timer_interrupt(void) {
     timestamp_t cur_tick = time_stamp();
     bool overflowed = cur_tick < last_tick;
 
-    //sync_acquire(callback_m); 
-    for (callback_t *p = callback_list; p; p = p->next) {
+    sync_acquire(callback_m); 
+    for (int i = 1; i <= MAX_CALLBACK_ID; i++) {
+        callback_t *p = &callback_arr[i];
         if (overflowed || p->next_timeout <= cur_tick) {
             p->fun(p->id, p->data);
-            if (max_tick - cur_tick < p->delay)
-                p->next_timeout = max_tick;
+            if (MAX_TICK - cur_tick < p->delay)
+                p->next_timeout = MAX_TICK;
             else
                 p->next_timeout = cur_tick + p->delay;
         }
-        if (!overflowed && p->next_timeout > cur_tick)
-            break;
     }
-    //sync_release(callback_m);
+    sync_release(callback_m);
 
     last_tick = cur_tick;
 
@@ -313,8 +105,8 @@ timestamp_t time_stamp(void) {
 }
 
 int stop_timer(void) {
-    cblist_destroy();
-    //sync_destroy_mutex(callback_m);
-    //sync_destroy_mutex(gid_m);
+    sync_destroy_mutex(callback_m);
+    initialized = false;
+    last_tick = 0;
     return 0;
 }
