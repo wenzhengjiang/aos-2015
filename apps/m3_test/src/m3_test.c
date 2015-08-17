e702ce02cf52f09a9339de1924aa75e4bd21f88f
Chris Mann <chris@bitpattern.com.au>
2015-08-17 17:06:23 +1000

    m3_test.

---
+++ apps/m3_test/src/m3_test.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+/****************************************************************************
+ *
+ *      $Id:  $
+ *
+ *      Description: Simple milestone 0 test.
+ *
+ *      Author:			Godfrey van der Linden
+ *      Original Author:	Ben Leslie
+ *
+ ****************************************************************************/
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("M3 TEST\n");
+    return 0;
+}
