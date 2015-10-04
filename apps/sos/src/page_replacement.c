bf993449bb647adfb3e3fc13809b1860b2ec2b56
Chris Mann <chris@bitpattern.com.au>
2015-10-04 14:08:09 +1100

    Page replacement from m7

--- apps/sos/src/page_replacement.c
+++ apps/sos/src/page_replacement.c
@@ -15,27 +15,44 @@
 #include <log/debug.h>
 #include <log/panic.h>
 
+/**
+ * Second chance page replacement algorithm.  Maintains a ref bit in the PTE,
+ * which tracks whether the page has been referenced since the last time the
+ * algorithm was invoked.  Evicted pages are unmapped from the process address
+ * space.
+ * @param as The address space for searching for victim pages
+ * @return PTE of the selected victim
+ */
 static pte_t* swap_choose_replacement_page(sos_addrspace_t* as) {
     pte_t* head = as->repllist_head;
-    int cnt = 0;
+    int err;
+    int loop_count = 0;
     while(1) {
-        if (head == as->repllist_head) cnt = 0;
-        dprintf(4, "[PRCLOCK] Tick %d\n", cnt++);
+        if (head == as->repllist_head) {
+            if (loop_count > 1) {
+                dprintf(1, "No pages left for eviction. Invoking 'OOM killer'\n");
+                process_delete(current_process());
+                longjmp(ipc_event_env, -1);
+            }
+            loop_count++;
+        }
+        printf("tick\n");
         if(as->repllist_head->pinned || as->repllist_head->swapd) {
-            dprintf(4, "[PRCLOCK] swapd %d,pinned %d\n", as->repllist_head->swapd, as->repllist_head->pinned);
             as->repllist_tail = as->repllist_head;
             as->repllist_head = as->repllist_head->next;
-            // TODO: There's potential for a hang here.  We should detect &
-            // kill client if it ends up with too many pages pinned, as we
-            // can't guarantee consistency should we start forcefully
-            // unpinning pages.
+            
             continue;
         }
         if(as->repllist_head->refd) {
             as->repllist_tail = as->repllist_head;
             as->repllist_head = as->repllist_head->next;
             seL4_ARM_Page_Unmap(as->repllist_tail->page_cap);
-            cspace_delete_cap(cur_cspace, as->repllist_tail->page_cap);
+            cspace_revoke_cap(cur_cspace, as->repllist_tail->page_cap);
+            err = cspace_delete_cap(cur_cspace, as->repllist_tail->page_cap);
+            if (err != CSPACE_NOERROR) {
+                ERR("[PR]: failed to delete page cap\n");
+            }
+            as->repllist_tail->page_cap = 0;
             as->repllist_tail->refd = false;
         } else {
             as->repllist_tail = as->repllist_head;
@@ -46,73 +63,101 @@ static pte_t* swap_choose_replacement_page(sos_addrspace_t* as) {
     assert(!"This can never happen");
 }
 
+/**
+ * Evict a page from the address space, swapping it out to 'disk'
+ * @param as Address space from which to evict
+ * @return Zero if successful, non-zero otherwise.
+ */
 int swap_evict_page(sos_addrspace_t *as) {
     sos_proc_t *proc = current_process();
-    pte_t *victim;
     dprintf(3, "[PR] EVICTING PAGE\n");
+
     if (!proc->cont.page_replacement_victim) {
         proc->cont.page_replacement_victim = swap_choose_replacement_page(as);
     }
-    if (!proc->cont.page_replacement_victim->swapd) {
-        victim = proc->cont.page_replacement_victim;
-        proc->cont.original_page_addr = LOAD_PAGE(proc->cont.page_replacement_victim->addr);
-        printf("Writing: %x, which gets converted to %x\n", victim->addr, LOAD_PAGE(victim->addr));
-        printf("Original page addr: %x\n", proc->cont.original_page_addr);
-        swap_addr saddr = sos_swap_write(LOAD_PAGE(victim->addr));
-        victim->addr = SAVE_PAGE(saddr);
-        victim->swapd = true;
-        printf("Writing: %x, %d\n", proc->cont.page_replacement_victim->addr , proc->cont.page_replacement_victim->swapd);
+
+    // Continuation
+    if (!proc->cont.page_replacement_victim->pinned) {
+        proc->cont.original_page_addr = LOAD_PAGE((seL4_Word)proc->cont.page_replacement_victim->addr);
+        swap_addr saddr = sos_swap_write(proc->cont.original_page_addr);
+        // Continuation
+        proc->cont.page_replacement_victim->addr = SAVE_PAGE(saddr);
+        proc->cont.page_replacement_victim->pinned = true;
+        // Wait on network irq
         longjmp(ipc_event_env, -1);
     }
+
+    // Continuation
     if (proc->cont.swap_status == SWAP_SUCCESS) {
         dprintf(4, "[PR] EVICTED. Tidying up.\n");
         assert(!proc->cont.page_replacement_victim->refd);
+        proc->cont.page_replacement_victim->swapd = true;
         proc->cont.page_replacement_victim->pinned = false;
         return 0;
+    } else if (proc->cont.swap_status == SWAP_FAILED) {
+        ERR("[PR] Deleting process due to swap failure\n");
+        process_delete(current_process());
+        return EIO;
     } else {
+        // Wait on network irq
         longjmp(ipc_event_env, -1);
     }
 }
 
+/**
+ * Predicate indicating whether a particular address in an address space is swaped.
+ * @param as the address space to check
+ * @param addr the virtual address in that address space.
+ * @return True if page containing addr is on disk, false otherwise.
+ */
 bool swap_is_page_swapped(sos_addrspace_t* as, client_vaddr addr) {
     pte_t *pt = as_lookup_pte(as, addr);
     return pt->swapd;
 }
 
 int swap_replace_page(sos_addrspace_t* as, client_vaddr readin) {
-    // TODO: Probably need to kill the process.  So much memory contention
-    // that we have no room to allocate ANY pages for the new process!
     dprintf(3, "[PR] STARTING PAGE REPLACEMENT\n");
     sos_proc_t *proc = current_process();
-    assert(as->repllist_head && as->repllist_tail);
-    swap_evict_page(as);
-    assert(proc->cont.page_replacement_victim->swapd);
-    pte_t *to_load = as_lookup_pte(as, readin);
-    if (!proc->cont.page_replacement_request) {
+
+    if (!proc->cont.page_replacement_victim || !proc->cont.page_replacement_victim->swapd) {
+        assert(as->repllist_head && as->repllist_tail);
+        swap_evict_page(as);
+        assert(proc->cont.page_replacement_victim->swapd);
+    }
+
+    dprintf(3, "[PR] replacement request %x\n", proc->cont.page_replacement_request);
+    if (proc->cont.page_replacement_request == 0) {
         proc->cont.page_replacement_request = readin;
-        if (to_load) {
-            dprintf(3, "[PR] READING in targeted replacement page\n");
-            assert(proc->cont.original_page_addr != 0);
-            memset((void*)proc->cont.original_page_addr, 0, PAGE_SIZE);
-            dprintf(4, "[PR] reading in new page %08x from address %u\n", readin, LOAD_PAGE(to_load->addr));
-            assert(proc->cont.page_replacement_victim->swapd);
-            printf("before read: Original page addr: %x\n", proc->cont.original_page_addr);
-            sos_swap_read(proc->cont.original_page_addr, LOAD_PAGE(to_load->addr));
-            longjmp(ipc_event_env, -1);
-            assert(proc->cont.original_page_addr != 0);
-        } else {
-            assert(!"Did not find page");
-        }
+        pte_t *to_load = as_lookup_pte(as, proc->cont.page_replacement_request);
+        assert(to_load);
+        assert(to_load->swapd);
+
+        dprintf(3, "[PR] READING in targeted replacement page\n");
+        assert(proc->cont.original_page_addr);
+        memset((void*)proc->cont.original_page_addr, 0, PAGE_SIZE);
+        dprintf(4, "[PR] reading in new page %08x from address %u\n", readin, LOAD_PAGE(to_load->addr));
+        assert(proc->cont.page_replacement_victim->swapd);
+
+        sos_swap_read(proc->cont.original_page_addr, LOAD_PAGE(to_load->addr));
+        longjmp(ipc_event_env, -1);
+        assert(proc->cont.original_page_addr != 0);
     }
     if (proc->cont.swap_status == SWAP_SUCCESS) {
+        pte_t *to_load = as_lookup_pte(as, proc->cont.page_replacement_request);
+        assert(to_load);
+        assert(to_load->swapd);
+
         dprintf(3, "[PR] REPLACEMENT COMPLETE\n");
-        printf("after: Original page addr: %x\n", proc->cont.original_page_addr);
         to_load->addr = SAVE_PAGE(proc->cont.original_page_addr);
+        assert(to_load->pinned == false);
         to_load->swapd = false;
-        assert(proc->cont.original_page_addr != 0x20627000);
-        seL4_CPtr fc = frame_cap(LOAD_PAGE(to_load->addr));
+        printf("OPR: %x\n", proc->cont.original_page_addr);
+        seL4_CPtr fc = frame_cap(proc->cont.original_page_addr);
         seL4_ARM_Page_Unify_Instruction(fc, 0, PAGE_SIZE);
-        //printf("NEW PAGE LOADED OKAY\n");
+        proc->cont.swap_status = 0;
+        proc->cont.page_replacement_request = 0;
+        proc->cont.original_page_addr = 0;
+        proc->cont.page_replacement_victim = NULL;
         return 0;
     } else {
         longjmp(ipc_event_env, -1);
