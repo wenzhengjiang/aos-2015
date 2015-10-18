32ef4222b8a7fcb3dc205427259c6beea3531ef1
Chris Mann <chris@bitpattern.com.au>
2015-10-18 17:57:44 +1100

    tty_test changes.

--- apps/tty_test/src/tty_test.c
+++ apps/tty_test/src/tty_test.c
@@ -28,43 +28,25 @@
 
 #include "ttyout.h"
 
-// Block a thread forever
-// we do this by making an unimplemented system call.
-static void
-thread_block(void){
-    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 1);
-    seL4_SetTag(tag);
-    seL4_SetMR(0, 1);
-    seL4_Call(SYSCALL_ENDPOINT_SLOT, tag);
-}
-
 int main(void){
     size_t count = 0;
-    const char* msg = "This is a message that's exactly 100 bytes long....................................................\n";
-    do {
-        count = printf("task:\tHello world, I'm\ttty_test!\n");
-        printf("count written: %d\n", count);
-        count = printf("Terminated early\n\0 DID NOT TERMINATE!\n");
-        printf("count written: %d\n", count);
-        count = printf("");
-        printf("count written: %d\n", count);
-        count = printf("%s%s", msg, msg);
-        printf("count written: %d\n", count);
-        count = printf("%s%s%s%s%s%s", msg, msg, msg, msg, msg, msg);
-        printf("count written: %d\n", count);
-        count = printf("\n");
-        count = printf("1\n");
-        count = printf("12\n");
-        count = printf("123\n");
-        count = printf("1234\n");
-        count = printf("12345\n");
-        count = printf("123456\n");
-        count = printf("1234567\n");
-        count = printf("12345678\n");
-        count = printf("123456789\n");
-        thread_block();
-        // sleep(1);	// Implement this as a syscall
-    } while(1);
-
+    count = printf("task:\tHello world, I'm\ttty_test!\n");
+    printf("count written: %d\n", count);
+    count = printf("Terminated early\nDID NOT TERMINATE!\n");
+    printf("count written: %d\n", count);
+    count = printf("1");
+    printf("count written: %d\n", count);
+    printf("count written: %d\n", count);
+    printf("count written: %d\n", count);
+    count = printf("\n");
+    count = printf("1\n");
+    count = printf("12\n");
+    count = printf("123\n");
+    count = printf("1234\n");
+    count = printf("12345\n");
+    count = printf("123456\n");
+    count = printf("1234567\n");
+    count = printf("12345678\n");
+    count = printf("123456789\n");
     return 0;
 }
